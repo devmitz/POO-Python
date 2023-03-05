0ae87f88129e0dc675b424dc65f41b162cb171f6
Felipe Schmitz <aryastarklinda@outlook.com>
2023-03-05 10:49:56 -0300

    att

--- avancando_no_poo/main.py
+++ avancando_no_poo/main.py
@@ -25,8 +25,8 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f"{self._nome} - {self.duracao}min - {self.ano} - {self._likes} Likes")
+    def __str__(self):
+        return f"{self._nome} - {self.duracao}min - {self.ano} - {self._likes} Likes"
 
 
 class Serie(Programa):
@@ -34,19 +34,36 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f"{self._nome} - {self.temporadas} temporadas - {self.ano} - {self._likes} Likes")
+    def __str__(self):
+        return f"{self._nome} - {self.temporadas} temporadas - {self.ano} - {self._likes} Likes"
 
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 interstelar = Filme("interestelar", 2014, 169)
 
 atlanta = Serie("atlanta", 2018, 2)
-lost_in_space = Serie("lost in space", 2018, 3)
 bones = Serie("bones", 2005, 12)
+lost_in_space = Serie("lost in space", 2018, 3)
 
-lista_series_filmes = [vingadores, interstelar, lost_in_space, bones, atlanta]
+lista_series_filmes = [interstelar, lost_in_space, bones, vingadores, atlanta]
 
+interstelar.dar_like()
+interstelar.dar_like()
+interstelar.dar_like()
+interstelar.dar_like()
 lost_in_space.dar_like()
 lost_in_space.dar_like()
 lost_in_space.dar_like()
@@ -54,6 +71,11 @@ bones.dar_like()
 bones.dar_like()
 bones.dar_like()
 
-for programa in lista_series_filmes:
-    programa.imprime()
+playlist_fim_de_semana = Playlist("fim de semana", lista_series_filmes)
 
+print(f"tamanho da playlist: {len(playlist_fim_de_semana)} ")
+
+for programa in playlist_fim_de_semana.listagem:
+    print(programa)
+
+print(f"Tá ou não tá? {interstelar in playlist_fim_de_semana}")
